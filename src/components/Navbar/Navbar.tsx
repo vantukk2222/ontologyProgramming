a051be32ac635551a7e645483f53ca5358ea2d33
nturion_lab <tunguyen@rion-lab.com>
2024-11-06 04:49:02 +0700

    update src svg

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
     return (
         <nav className="flex flex-row bg-white justify-between flex-nowrap p-4 items-center w-full h-fit lg:max-w-[90rem] mx-auto px-4 md:px-8 border-2 border-bottom border-b-slate-200 drop-shadow-sm">
             <a className=" flex flex-row rounded-md focus:outline-none justify-center items-center focus:opacity-80" href="/home">
-            <img src="/assets/svg/book.svg" alt="Book Icon" className="w-28 h-12" />
+            <img src="/svg/book.svg" alt="Book Icon" className="w-28 h-12" />
               <span className="text-lg font-bold">Chương trình đào tạo</span>
             </a>
             {/* <Search/> */}
